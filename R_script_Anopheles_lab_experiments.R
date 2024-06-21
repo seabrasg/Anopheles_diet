e608030e2d34366946e47067baf601ba28ea5a06
seabrasg <59282831+seabrasg@users.noreply.github.com>
2024-06-21 19:10:06 +0100

    Add files via upload

--- R_script_Anopheles_lab_experiments.R
+++ R_script_Anopheles_lab_experiments.R
@@ -17,11 +17,13 @@ library(openxlsx)
 library(nlme)
 library(FSA)
 library(tidyverse)
+library(ggpubr)
 library(rstatix)
 library(broom)
 library(dplyr)
+library(plyr)
 library(forcats)
-
+library(cowplot)
 
 ## Set the working directory
 setwd('')
@@ -598,6 +600,274 @@ print(mwu_test_result)
 sink()
 
 
+############################################################################
+##########################################################################
+############################################################################
+##########################################################################
+# PLOTS MANUSCRIPT
+############################################################################
+##########################################################################
+
+
+
+###########################################################
+# Plot Pfeeding + Meal preference
+
+png(file = "./plot_feeding_mealpreference.png", bg="white",width = 8, height = 4,  units = "in", res = 300)
+
+df<-read.xlsx("./dados_bloodmeal_appetite.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+
+colnames(df)
+
+df$Generation <- factor(df$Generation, levels = unique(df$Generation))
+
+df$Diet <- factor(df$Diet, levels = unique(df$Diet))
+
+# Calculate the average PFeeding values and standard deviation
+avg_cq <- aggregate(PFeeding ~ Generation + Diet, data = df, FUN = mean)
+std_dev <- aggregate(PFeeding ~ Generation + Diet, data = df, FUN = sd)
+
+# Merge average PFeeding and standard deviation data
+avg_stdev <- merge(avg_cq, std_dev, by = c("Generation", "Diet"))
+
+
+# Create the grouped bar chart
+
+p1 <- ggplot(avg_stdev, aes(x = Generation, y = PFeeding.x, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(y = PFeeding.x, ymin = PFeeding.x + PFeeding.y, ymax = PFeeding.x + PFeeding.y), 
+                position = position_dodge(width = 0.9), width = 0.25, colour="#636363") +
+  geom_linerange(aes(ymin = PFeeding.x, ymax = PFeeding.x + PFeeding.y),position = position_dodge(width = 0.9), colour="#636363") +
+  scale_fill_manual(name = "Type of diet", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Percentage of females feeding \n on mouse",tag="A") +
+  theme_minimal()
+
+
+##############################################
+##############################################
+# Meal preference
+
+df<-read.xlsx("./Dados_meal_preference.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+
+colnames(df)
+
+
+#######################
+# Plot Meal preference
+
+df$Diet <- factor(df$Diet, levels = unique(df$Diet))
+
+df$Outcome <- factor(df$Outcome, levels = c("red", "blue", "none"))
+
+# Create the grouped bar chart
+p2 <- ggplot(df, aes(x = Replicate, y = PFeeding, fill=Outcome)) +
+  geom_bar(position="fill", stat = "identity") +
+  scale_fill_manual(name = "Preference", labels = c("Blood", "BLOODless","none"), values =c("#d6604d", "#fee090","#bdbdbd")) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(title = "",
+       x = "Experiments", y = "Percentage of Feeding",tag="B") +
+  facet_wrap( ~ Diet, ncol=2, strip.position = "top") +
+  theme_minimal() + theme(axis.text.x=element_blank(), axis.ticks.x=element_blank())
+
+p_all <- plot_grid(p1, p2, nrow=1, ncol=2)
+
+p_all
+
+dev.off()
+
+
+###########################################################
+###########################################################
+# LONGEVITY + WING LENGTH
+png(file = "./plot_longevity_winglength.png", bg="white",width = 7, height = 5,  units = "in", res = 300)
+
+df<-read.xlsx("./dados_longevidade.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+colnames(df)
+#######################
+# Plot Longevity
+df$Generation <- factor(df$Generation, levels = unique(df$Generation))
+
+df$Diet <- factor(df$Diet, levels = unique(df$Diet))
+
+df$Sex <- factor(df$Sex, levels = unique(df$Sex))
+levels(df$Sex)
+levels(df$Sex) <- c("Female", "Male")
+levels(df$Sex)
+
+# Create the grouped bar chart
+
+p1 <- ggplot(df, aes(x = Generation, y = average_life_span, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(y = average_life_span, ymin = average_life_span + SD, ymax = average_life_span + SD), 
+                position = position_dodge(width = 0.9), width = 0.25, colour="#636363") +
+  geom_linerange(aes(ymin = average_life_span, ymax = average_life_span + SD),position = position_dodge(width = 0.9), colour="#636363") +
+  scale_fill_manual(name = "Type of diet", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Longevity (number of days)",tag="A") +
+  facet_wrap( ~ Sex, ncol=1, strip.position = "top") +
+  theme_minimal() +  theme(legend.position="none") 
+
+###########################################################
+###########################################################
+# WING LENGTH
+
+df<-read.xlsx("./dados_wing_length.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+colnames(df)
+
+#######################
+# Plot Wing length
+
+df$Generation <- factor(df$Generation, levels = unique(df$Generation))
+
+df$Diet <- factor(df$Diet, levels = unique(df$Diet))
+
+df$Sex <- factor(df$Sex, levels = unique(df$Sex))
+levels(df$Sex)
+levels(df$Sex) <- c("Female", "Male")
+levels(df$Sex)
+
+# Create the grouped bar chart
+p2 <- ggplot(df, aes(x = Generation, y = AVERAGE, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  geom_errorbar(aes(y = AVERAGE, ymin = AVERAGE + STDEV, ymax = AVERAGE + STDEV), 
+                position = position_dodge(width = 0.9), width = 0.25, colour="#636363") +
+  geom_linerange(aes(ymin = AVERAGE, ymax = AVERAGE + STDEV),position = position_dodge(width = 0.9), colour="#636363") +
+  scale_fill_manual(name = "Type of diet", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Wing length",tag="B") +
+  facet_wrap( ~ Sex, ncol=1, strip.position = "top") +
+  theme_minimal() +  theme(legend.position="none") 
+
+p_all <- plot_grid(p1, p2, nrow=1, ncol=2,
+                   label_size = 14,
+                   label_x = 0.92, label_y = 0.75,
+                   hjust = -0.5, vjust = -0.5,
+                   align="v")
+
+legend <- get_legend(p1 + 
+                       guides(color = guide_legend(nrow = 1)) +
+                       theme(legend.position = "bottom"))
+
+
+plot_grid(p_all, legend, ncol = 1, rel_heights = c(1, .1))
+
+dev.off()
+
+
+############################
+############################
+# PLOT Infection rate + Infection intensity
+
+df<-read.xlsx("./dados_infection.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+colnames(df)
+
+png(file = "./plot_infection_rate_and_intensity.png", bg="white",width = 5, height = 8,  units = "in", res = 300)
+
+p1 <- ggplot(df, aes(x = as.factor(Generation), y = Infection_Rate, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(name = "Type of meal", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Infection rate",tag="A") +
+  theme_minimal() +  theme(legend.position="none") 
+
+
+p2 <- ggplot(df, aes(x = as.factor(Generation), y = Infection_Intensity, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(name = "Type of meal", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Infection intensity",tag="B") +
+  theme_minimal() +  theme(legend.position="none") 
+
+library(cowplot)
+p_all <- plot_grid(p1, p2, nrow=2, ncol=1,
+                   label_size = 14,
+                   label_x = 0.92, label_y = 0.75,
+                   hjust = -0.5, vjust = -0.5,
+                   align="v")
+
+legend <- get_legend(p1 + 
+                       guides(color = guide_legend(nrow = 1)) +
+                       theme(legend.position = "bottom"))
+
+plot_grid(p_all, legend, ncol = 1, rel_heights = c(1, .1))
+
+
+get_legend_custom <- function(myggplot) {
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+# Extract the legend from p1
+legend <- get_legend_custom(p1)
+
+# Display the legend
+plot_grid(NULL, legend, ncol = 1, rel_heights = c(1, 0.1))
+
+dev.off()
+
+
+
+#################
+# NEW PLOT Infection rate and intensity (individual dots)
+#################
+png(file = "./plot_infection_rate_and_intensity_jitter.png", bg="white",width = 5, height = 6,  units = "in", res = 300)
+
+# Infection rate
+df<-read.xlsx("./dados_infection.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+colnames(df)
+
+p1 <- ggplot(df, aes(x = as.factor(Generation), y = Infection_Rate, fill = Diet)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_fill_manual(name = "Type of meal", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090")) +
+  labs(title = "",
+       x = "Generation", y = "Infection rate",tag="A") +
+  theme_minimal() +  theme(legend.position="none") 
+
+
+df<-read.xlsx("./dados_oocyst_number.xlsx", sheet = 1, startRow = 1, colNames = TRUE)
+colnames(df)
+
+# Calculate the average OocystNumber values and standard deviation
+avg_cq <- aggregate(OocystNumber ~ Generation + Diet, data = df, FUN = mean)
+std_dev <- aggregate(OocystNumber ~ Generation + Diet, data = df, FUN = sd)
+
+# Merge average PFeeding and standard deviation data
+avg_stdev <- merge(avg_cq, std_dev, by = c("Generation", "Diet"))
+
+colnames(avg_stdev)=c("Generation","Diet","Oocyst_mean", "Ooscyt_sd")
+
+avg_stdev$Diet <- factor(avg_stdev$Diet, levels = unique(avg_stdev$Diet))
+levels(avg_stdev$Diet)
+avg_stdev$Generation <- factor(avg_stdev$Generation, levels = unique(avg_stdev$Generation))
+levels(avg_stdev$Generation)
+avg_stdev$Oocyst_mean <- as.numeric(avg_stdev$Oocyst_mean)
+avg_stdev$Ooscyt_sd <- as.numeric(avg_stdev$Ooscyt_sd)
+
+
+df$Group <- factor(df$Group, levels = c("B_F23","D_F23","B_F27","D_F27","B_F38","D_F38"))
+
+p2 <- ggplot(df, aes(x = as.factor(Generation), y = OocystNumber, fill = as.factor(Diet))) +
+  geom_jitter(position= position_jitterdodge(jitter.width = 0.5, dodge.width = 0.6),pch=21, stroke=0, size=2) +
+  scale_fill_manual(name = "Type of diet", labels = c("Blood", "BLOODless"), values =c("#d6604d", "#fee090"))+
+  stat_summary(fun=median, geom ="crossbar", position = position_dodge(width = 0.6), 
+               width = 0.4, size=0.2, show.legend = FALSE) +
+  labs(title = "",
+       x = "Generation", y = "Oocyst Number",tag="B") +
+  theme_minimal() +  theme(legend.position="bottom") 
+
+
+library(cowplot)
+p_all <- plot_grid(p1, p2, nrow=2, ncol=1,
+                   label_size = 14,
+                   label_x = 0.92, label_y = 0.75,
+                   hjust = -0.5, vjust = -0.5,
+                   align="v", rel_heights = c(1, 1))
+p_all
+
+dev.off()
 
 
 
